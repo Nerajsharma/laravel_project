24d0e3bab01e1ccf5b28da45e28a7639e219c41d
Nerajsharma <nrsharma123g@gmail.com>
2024-07-16 10:38:10 +0545

    create a project

---
+++ resources/views/help.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>help</title>
+    @vite(['resources/css/style.css', 'resources/css/nb.css'])
+</head>
+
+<body>
+    @include('navbar')
+
+    <h1>hellow everyone i am help page</h1>
+</body>
+
+</html>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar">
+    <ul><a href="{{route('home')}}">
+            <li class="active">Home</li>
+        </a>
+        <a href="{{route('contact')}}">
+            <li>Contact</li>
+        </a>
+        <a href="{{route('about')}}">
+            <li>about</li>
+        </a>
+        <a href="{{route('help')}}">
+            <li>help</li>
+        </a>
+    </ul>
+</nav>
